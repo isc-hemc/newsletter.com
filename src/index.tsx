f0f8da3c43d6ea50d5e2cf206e5dd344c917abd3
David Martinez <davidmartinez@MacBook-Pro-de-David.local>
2023-04-01 16:49:15 -0600

    chore: prepare aplication for development

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import { StrictMode } from 'react';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <StrictMode>
+    <h1>Stori | Newsletter</h1>
+  </StrictMode>
+);
+
+reportWebVitals();

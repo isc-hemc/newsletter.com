66e4ff3ac2f30ff54124c3945e4a7a715f0fa183
David Martinez <davestringm@gmail.com>
2023-04-03 21:49:28 -0600

    feat: allow to use tailwind inside src folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+const theme = require('./src/assets/js/theme');
+
 module.exports = {
   content: ['./src/**/*.{jsx,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          50: '#B7C8E6',
-          100: '#9EAFCD',
-          200: '#8596B4',
-          300: '#6B7C9A',
-          400: '#526381',
-          500: '#384967',
-          600: '#2B3C5A',
-          700: '#1F304E',
-          800: '#122341',
-        },
-      },
-    },
-  },
+  theme,
   plugins: [],
 };

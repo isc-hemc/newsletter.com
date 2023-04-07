42348fc66a7c24454fd2f6bd4e751827ca1127e9
David Martinez <davestringm@gmail.com>
2023-04-07 00:44:45 -0600

    feat: improve not-found pages styles

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,7 @@
+import { css, cx } from '@emotion/css';
 import { NewsletterProvider } from 'providers';
+import { useMemo } from 'react';
+import { use100vh } from 'react-div-100vh';
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
 
@@ -7,7 +10,17 @@ import { Header } from './components/Header';
 import { IdleScreen } from './screens/Idle';
 
 export const Home = (): JSX.Element => {
+  const height = use100vh();
+
   const { t } = useTranslation('page:home');
+
+  const HEIGHT_CLASS = useMemo(
+    () =>
+      css`
+        height: ${height}px;
+      `,
+    [height],
+  );
 
   return (
     <>
@@ -16,7 +29,7 @@ export const Home = (): JSX.Element => {
       </Helmet>
 
       <NewsletterProvider>
-        <main className="relative flex h-screen flex-col">
+        <main className={cx('relative flex flex-col', HEIGHT_CLASS)}>
           <Header />
 
           <section className="mx-auto w-full max-w-3xl flex-1 px-4 pb-20 pt-4">

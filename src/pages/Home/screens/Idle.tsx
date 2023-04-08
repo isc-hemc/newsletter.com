a3a25d2a2c4bc462f9e0fcfc23f987d138ad6d5c
David Martinez <davestringm@gmail.com>
2023-04-08 14:03:53 -0600

    feat: improve the idle screen layout

--- src/pages/Home/screens/Idle.tsx
+++ src/pages/Home/screens/Idle.tsx
@@ -1,7 +1,26 @@
 import { H1, H2 } from 'components/typography';
 import { NewsletterContext } from 'contexts';
+import { i18n } from 'locales';
 import { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
+import { MdOutlineTimer } from 'react-icons/md';
+import { RiMailSettingsLine } from 'react-icons/ri';
+
+import { Card } from '../components/Card';
+
+const MANUAL_SETUP_BENEFITS = [
+  i18n.t('screen.idle.manual-setup.create-template', { ns: 'page:home' }),
+  i18n.t('screen.idle.manual-setup.upload-contacts', { ns: 'page:home' }),
+  i18n.t('screen.idle.manual-setup.config-newsletter', { ns: 'page:home' }),
+  i18n.t('screen.idle.manual-setup.upload-attachment', { ns: 'page:home' }),
+];
+
+const QUICK_START_BENEFITS = [
+  i18n.t('screen.idle.quick-start.select-template', { ns: 'page:home' }),
+  i18n.t('screen.idle.quick-start.select-contacts', { ns: 'page:home' }),
+  i18n.t('screen.idle.quick-start.review-newsletter', { ns: 'page:home' }),
+  i18n.t('screen.idle.quick-start.less-than-a-minute', { ns: 'page:home' }),
+];
 
 export const IdleScreen = (): JSX.Element => {
   const context = useContext(NewsletterContext);
@@ -11,15 +30,25 @@ export const IdleScreen = (): JSX.Element => {
   return (
     <>
       <H1 className="mb-2 text-center uppercase">{t('screen.idle.title')}</H1>
-      <H2 className="text-center">{t('screen.idle.subtitle')}</H2>
 
-      <div className="mt-4 flex justify-between gap-2">
-        <button onClick={() => context?.send('MANUAL_SETUP')} type="button">
-          MANUAL_SETUP
-        </button>
-        <button onClick={() => context?.send('QUICK_START')} type="button">
-          QUICK_START
-        </button>
+      <H2 className="mb-8 text-center">{t('screen.idle.subtitle')}</H2>
+
+      <div className="flex flex-col gap-4 md:flex-row">
+        <Card
+          benefits={MANUAL_SETUP_BENEFITS}
+          className="flex-1"
+          icon={RiMailSettingsLine}
+          onClick={() => context?.send('MANUAL_SETUP')}
+          title={t('screen.idle.manual-setup.title')}
+        />
+
+        <Card
+          benefits={QUICK_START_BENEFITS}
+          className="flex-1"
+          icon={MdOutlineTimer}
+          onClick={() => context?.send('QUICK_START')}
+          title={t('screen.idle.quick-start.title')}
+        />
       </div>
     </>
   );
